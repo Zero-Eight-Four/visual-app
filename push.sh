51ccd79949171408ce4c8a5ce4d93016d2fc52c1
ZeroEF <2730843725@qq.com>
2025-12-19 18:40:23 +0800

    1.5

--- push.sh
+++
@@ -1,2 +0,0 @@
-docker tag visual-app-backend:latest registry.cn-hangzhou.aliyuncs.com/zjyp/java-app:visual-app-backend
-docker push registry.cn-hangzhou.aliyuncs.com/zjyp/java-app:visual-app-backend
